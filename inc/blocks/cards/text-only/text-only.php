3df5d80255e43461df872ce3250ef6cd7c2bbce8
krishaiquin <krishaiquin@gmail.com>
2025-07-14 12:59:15 -0700

    remove unnecessary card blocks

--- inc/blocks/cards/text-only/text-only.php
+++ inc/blocks/cards/text-only/text-only.php
@@ -1,6 +1,6 @@
-<!-- wp:group {"className":"card card\u002d\u002dgray "} -->
-<div class="wp-block-group card card--gray"><!-- wp:heading -->
-	<h2 class="wp-block-heading">Light Grey Text Only Content card</h2>
+<!-- wp:group {"className":"card "} -->
+<div class="wp-block-group card"><!-- wp:heading -->
+	<h2 class="wp-block-heading">Text-Only card</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:paragraph -->
